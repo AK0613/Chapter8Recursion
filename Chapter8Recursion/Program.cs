eaa33618c11494c92c20e583eba4479df08bf891
Albert Montufar <Albert Montufar@LAPTOP-UKGPBO5K>
2022-02-28 14:51:26 -0800

    Implemented first Problem

--- Chapter8Recursion/Program.cs
+++ Chapter8Recursion/Program.cs
@@ -19,27 +19,27 @@ namespace Chapter8Recursion
                     case 1:
                         P1();
                         break;
-                    case 2:
-                        p2();
-                        break;
-                    case 3:
-                        p3();
-                        break;
-                        /*case 5:
-                            p5();
+                        /*case 2:
+                            p2();
                             break;
-                        case 6:
-                            p6();
+                        case 3:
+                            p3();
                             break;
-                        case 7:
-                            p7();
-                            break;
-                        case 8:
-                            p8();
-                            break;
-                        case 9:
-                            p9();
-                            break;*/
+                            case 5:
+                                p5();
+                                break;
+                            case 6:
+                                p6();
+                                break;
+                            case 7:
+                                p7();
+                                break;
+                            case 8:
+                                p8();
+                                break;
+                            case 9:
+                                p9();
+                                break;*/
                 }
 
 
@@ -50,11 +50,8 @@ namespace Chapter8Recursion
             } while (choice == "y" || choice == "Y");
         }
 
-        private static void P1()
-        {
-            throw new NotImplementedException();
-        }
 
+        #region Menu
         static int Menu()
         {
             string? input;
@@ -79,5 +76,46 @@ namespace Chapter8Recursion
 
             return problem;
         }
+        #endregion
+
+        #region Problems
+
+        private static void P1()
+        {
+            int numOfSteps;
+            string? input;
+            bool valid;
+
+            Console.Clear();
+            Console.WriteLine("Problem 1: A child is running up a staircase with n steps and can hop either 1, 2, or 3, steps at a time. Implement a method to count how many possible ways the child can run up the stairs");
+
+            do
+            {
+                Console.Write("Enter an integer value for n: ");
+                input = Console.ReadLine();
+
+                valid = int.TryParse(input, out numOfSteps);
+
+                if (!valid)
+                    Console.WriteLine("Invalid input.");
+
+            } while (!valid);
+
+            Console.WriteLine("The result is: ");
+            int numSteps = Steps(numOfSteps);
+            Console.WriteLine(numSteps);
+
+        }
+
+        static int Steps(int n)
+        {
+            if (n < 0)
+                return 0;
+            else if (n == 0)
+                return 1;
+            else
+                return Steps(n-1) + Steps(n-2) + Steps(n-3);    
+        }
+        #endregion
     }
 }
